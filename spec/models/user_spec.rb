3d424c60cd9995b84fb533a6d5640bbbc41e48d4
Szarad <bozek.karolina@gmail.com>
2015-04-14 12:31:29 +0000

    globalization of test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
 describe User do
+  before :each do
+    @user=user =User.new(first_name: 'John', last_name:'Smith')
+  end
+    
   describe 'full name' do
     it 'returns user full name' do
-      user =User.new(first_name: 'John', last_name:'Smith')
       
-      expect(user.full_name).to eq 'John Smith'
+      expect(@user.full_name).to eq 'John Smith'
     end
   end
   
   
   describe 'initials' do
     it 'returns user initials' do
-     user =User.new(first_name: 'John', last_name:'Smith')
-      
-     expect(user.initials).to eq 'JS'
+       
+     expect(@user.initials).to eq 'JS'
     end
   end
 
