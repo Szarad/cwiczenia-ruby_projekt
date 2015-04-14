fa194d44e1210eea6292f780ce546caa8653ff2b
Szarad <bozek.karolina@gmail.com>
2015-04-14 12:28:00 +0000

    add new function initials for User and test for this function

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,18 @@ describe User do
   describe 'full name' do
     it 'returns user full name' do
       user =User.new(first_name: 'John', last_name:'Smith')
+      
       expect(user.full_name).to eq 'John Smith'
     end
   end
+  
+  
+  describe 'initials' do
+    it 'returns user initials' do
+     user =User.new(first_name: 'John', last_name:'Smith')
+      
+     expect(user.initials).to eq 'JS'
+    end
+  end
+
 end
