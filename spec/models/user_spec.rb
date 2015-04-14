db1c91aa3f3117cd4efa799909d74633ad28ebbd
Szarad <bozek.karolina@gmail.com>
2015-04-14 13:13:20 +0000

    add gem factory_girls and test for user using factories

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,13 @@ describe User do
   it {should validate_presence_of :first_name}
   
   before :each do
-    @user=user =User.new(first_name: 'John', last_name:'Smith')
+    @user=create(:user)
   end
     
   describe 'full name' do
     it 'returns user full name' do
       
-      expect(@user.full_name).to eq 'John Smith'
+      expect(@user.full_name).to eq @user.first_name+' '+@user.last_name
     end
   end
   
